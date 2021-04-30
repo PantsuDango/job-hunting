538b107042ce6a9a4a32796efa25b5cb78f97642
bard <13266871263@163.com>
2021-04-30 11:31:30 +0800

    init project

--- git.sh
+++ git.sh
@@ -6,17 +6,17 @@ path=$PWD
 cd $path/service
 num=$(netstat -nlpt | grep ":::3000" | grep -v "grep" | wc -l)
 if [ $num -eq 1 ]; then
-    kill -9 $(ps -ef | grep "recruitment" | grep -v "grep" | awk '{print $2}')
+    kill -9 $(ps -ef | grep "job-hunting" | grep -v "grep" | awk '{print $2}')
     sleep 3
 fi
-nohup go run recruitment.go &
+nohup go run job-hunting.go &
 sleep 3
 
 num=$(netstat -nlpt | grep ":::3000" | grep -v "grep" | wc -l)
 if [ $num -eq 1 ]; then
-    echo "run recruitment success"
+    echo "run job-hunting success"
 else
     cd $path/app
     tail -n 10 nohup.out
-    echo "run recruitment fail"
+    echo "run job-hunting fail"
 fi
