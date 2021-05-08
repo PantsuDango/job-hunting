b3a9278cbf35bbdcc6d9b8a821e0a6bd74928cc2
PantsuDango <394883561@qq.com>
2021-05-09 00:09:41 +0800

    添加 ListJob 接口

--- model/result/result.go
+++ model/result/result.go
@@ -3,6 +3,6 @@ package result
 import "job-hunting/model/params"
 
 type ListJob struct {
-	ListJob []params.JobParams `json:"ListJob"`
 	Count   int                `json:"Count"`
+	ListJob []params.JobParams `json:"ListJob"`
 }
